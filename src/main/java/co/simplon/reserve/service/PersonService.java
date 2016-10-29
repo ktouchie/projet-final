faaff5ce5d9e40e3ea9a5ca9d50172931a60dec4
Karin Touchie <ktouchie@gmail.com>
2016-10-29 12:18:43 +0200

    Add methods to PersonService

--- src/main/java/co/simplon/reserve/service/PersonService.java
+++ src/main/java/co/simplon/reserve/service/PersonService.java
@@ -1,8 +1,12 @@
 package co.simplon.reserve.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
 
+import co.simplon.reserve.model.Person;
 import co.simplon.reserve.repository.PersonRepository;
 
 @Repository
@@ -10,5 +14,28 @@ public class PersonService {
 
     @Autowired
     public PersonRepository personRepository;
+
+    public List<Person> getAll() {
+	return personRepository.findAll();
+    }
+
+    public void delete(Integer id) {
+	personRepository.delete(id);
+    }
+
+    public Person add(Person person) {
+	return personRepository.save(person);
+    }
+
+    public Person getById(int id) {
+	return personRepository.findOne(id);
+    }
+
+    public List<Person> findByName(String name) {
+	Person person = new Person(name, null);
+	Example<Person> example = Example.of(person);
+	return personRepository.findAll(example);
+
+    }
 
 }
