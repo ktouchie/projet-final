2e8ef98a7bca0e6a3430255ff7fff677b8ffda65
Karin Touchie <ktouchie@gmail.com>
2016-10-30 13:24:04 +0100

    Change Reservation constructors to take User, Computer, Room objects instead of IDs

--- src/main/java/co/simplon/reserve/web/ReservationController.java
+++ src/main/java/co/simplon/reserve/web/ReservationController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import co.simplon.reserve.model.Computer;
 import co.simplon.reserve.model.Reservation;
+import co.simplon.reserve.model.Room;
+import co.simplon.reserve.model.User;
 import co.simplon.reserve.service.ReservationService;
 
 @Controller
@@ -29,27 +32,27 @@ public class ReservationController {
 
     @RequestMapping("/addReservation")
     public ModelAndView addReservation(@RequestParam("startTime") LocalDateTime startTime,
-	    @RequestParam("endTime") LocalDateTime endTime, @RequestParam("idComputer") Integer idComputer,
-	    @RequestParam("idRoom") Integer idRoom, @RequestParam("idUser") Integer idUser, ModelMap model) {
-	Reservation reservation = new Reservation(startTime, endTime, idComputer, idRoom, idUser);
+	    @RequestParam("endTime") LocalDateTime endTime, @RequestParam("computer") Computer computer,
+	    @RequestParam("room") Room room, @RequestParam("user") User user, ModelMap model) {
+	Reservation reservation = new Reservation(startTime, endTime, computer, room, user);
 	reservationService.add(reservation);
 	return new ModelAndView("redirect:/reservations");
     }
 
     @RequestMapping("/addReservation")
     public ModelAndView addReservation(@RequestParam("startTime") LocalDateTime startTime,
-	    @RequestParam("endTime") LocalDateTime endTime, @RequestParam("idRoom") Integer idRoom,
-	    @RequestParam("idUser") Integer idUser, ModelMap model) {
-	Reservation reservation = new Reservation(startTime, endTime, idRoom, idUser);
+	    @RequestParam("endTime") LocalDateTime endTime, @RequestParam("room") Room room,
+	    @RequestParam("user") User user, ModelMap model) {
+	Reservation reservation = new Reservation(startTime, endTime, room, user);
 	reservationService.add(reservation);
 	return new ModelAndView("redirect:/reservations");
     }
 
     @RequestMapping("/addReservation")
-    public ModelAndView addReservation(@RequestParam("idComputer") Integer idComputer,
-	    @RequestParam("idUser") Integer idUser, @RequestParam("startTime") LocalDateTime startTime,
-	    @RequestParam("endTime") LocalDateTime endTime, ModelMap model) {
-	Reservation reservation = new Reservation(idComputer, idUser, startTime, endTime);
+    public ModelAndView addReservation(@RequestParam("computer") Computer computer, @RequestParam("user") User user,
+	    @RequestParam("startTime") LocalDateTime startTime, @RequestParam("endTime") LocalDateTime endTime,
+	    ModelMap model) {
+	Reservation reservation = new Reservation(startTime, endTime, computer, user);
 	reservationService.add(reservation);
 	return new ModelAndView("redirect:/reservations");
     }
