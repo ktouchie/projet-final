895fc31084ec069de38de18a092847c4735818b5
Karin Touchie <ktouchie@gmail.com>
2016-11-07 17:29:49 +0100

    Fix errors in Reservation controller

--- src/main/java/co/simplon/reserve/model/Reservation.java
+++ src/main/java/co/simplon/reserve/model/Reservation.java
@@ -1,7 +1,8 @@
 package co.simplon.reserve.model;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,46 +17,56 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private LocalDateTime startTime;
+    private Date startTime;
 
-    private LocalDateTime endTime;
-    
-    @ManyToOne
-    @JoinColumn(name = "computer_id")
+    private Date endTime;
+
+    @ManyToOne(cascade = { CascadeType.MERGE }, targetEntity = Computer.class)
+    @JoinColumn(name = "computerId")
     private Computer computer;
-    
-    @ManyToOne
-    @JoinColumn(name = "room_id")
+
+    @ManyToOne(cascade = { CascadeType.MERGE }, targetEntity = Room.class)
+    @JoinColumn(name = "roomId")
     private Room room;
-    
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+
+    @ManyToOne(cascade = { CascadeType.MERGE }, targetEntity = User.class)
+    @JoinColumn(name = "userId")
     private User user;
 
     public Reservation() {
 
     }
 
-    public Reservation(LocalDateTime startTime, LocalDateTime endTime, Computer computer, User user) {
-	this.startTime = startTime;
-	this.endTime = endTime;
-	this.computer = computer;
-	this.user = user;
-    }
-
-    public Reservation(LocalDateTime startTime, LocalDateTime endTime, Room room, User user) {
-	this.startTime = startTime;
-	this.endTime = endTime;
-	this.room = room;
-	this.user = user;
-    }
-
-    public Reservation(LocalDateTime startTime, LocalDateTime endTime, Computer computer, Room room, User user) {
+    public Reservation(Date startTime, Date endTime, Computer computer, Room room, User user) {
 	this.startTime = startTime;
 	this.endTime = endTime;
 	this.computer = computer;
 	this.room = room;
 	this.user = user;
+    }
+
+    public Integer getId() {
+	return id;
+    }
+
+    public Date getStartTime() {
+	return startTime;
+    }
+
+    public Date getEndTime() {
+	return endTime;
+    }
+
+    public Computer getComputer() {
+	return computer;
+    }
+
+    public Room getRoom() {
+	return room;
+    }
+
+    public User getUser() {
+	return user;
     }
 
 }
--- src/main/java/co/simplon/reserve/web/ReservationController.java
+++ src/main/java/co/simplon/reserve/web/ReservationController.java
@@ -1,9 +1,11 @@
 package co.simplon.reserve.web;
 
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,45 +16,54 @@ import co.simplon.reserve.model.Computer;
 import co.simplon.reserve.model.Reservation;
 import co.simplon.reserve.model.Room;
 import co.simplon.reserve.model.User;
+import co.simplon.reserve.service.ComputerService;
 import co.simplon.reserve.service.ReservationService;
+import co.simplon.reserve.service.RoomService;
+import co.simplon.reserve.service.UserService;
 
 @Controller
 @RequestMapping
 public class ReservationController {
 
     @Autowired
+    private ComputerService computerService;
+
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
     private ReservationService reservationService;
 
     @RequestMapping("/reservations")
     public ModelAndView getAll(ModelMap model) {
+	List<Computer> computerList = computerService.getAll();
+	model.addAttribute("computerList", computerList);
+	List<User> userList = userService.getAll();
+	model.addAttribute("userList", userList);
+	List<Room> roomList = roomService.getAll();
+	model.addAttribute("roomList", roomList);
 	List<Reservation> reservationList = reservationService.getAll();
 	model.addAttribute("reservationList", reservationList);
+	Date date = new Date();
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH");
+	String today = sdf.format(date);
+	model.addAttribute("today", today);
 	return new ModelAndView("reservations", model);
     }
 
     @RequestMapping("/addReservation")
-    public ModelAndView addReservation(@RequestParam("startTime") LocalDateTime startTime,
-	    @RequestParam("endTime") LocalDateTime endTime, @RequestParam("computer") Computer computer,
-	    @RequestParam("room") Room room, @RequestParam("user") User user, ModelMap model) {
+    public ModelAndView addReservation(
+	    @RequestParam("startTime") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH") Date startTime,
+	    @RequestParam("endTime") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH") Date endTime,
+	    @RequestParam("computerId") Integer computerId, @RequestParam("roomId") Integer roomId,
+	    @RequestParam("userId") Integer userId, ModelMap model) {
+	Computer computer = computerService.getById(computerId);
+	Room room = roomService.getById(roomId);
+	User user = userService.getById(userId);
 	Reservation reservation = new Reservation(startTime, endTime, computer, room, user);
-	reservationService.add(reservation);
-	return new ModelAndView("redirect:/reservations");
-    }
-
-    @RequestMapping("/addReservation")
-    public ModelAndView addReservation(@RequestParam("startTime") LocalDateTime startTime,
-	    @RequestParam("endTime") LocalDateTime endTime, @RequestParam("room") Room room,
-	    @RequestParam("user") User user, ModelMap model) {
-	Reservation reservation = new Reservation(startTime, endTime, room, user);
-	reservationService.add(reservation);
-	return new ModelAndView("redirect:/reservations");
-    }
-
-    @RequestMapping("/addReservation")
-    public ModelAndView addReservation(@RequestParam("computer") Computer computer, @RequestParam("user") User user,
-	    @RequestParam("startTime") LocalDateTime startTime, @RequestParam("endTime") LocalDateTime endTime,
-	    ModelMap model) {
-	Reservation reservation = new Reservation(startTime, endTime, computer, user);
 	reservationService.add(reservation);
 	return new ModelAndView("redirect:/reservations");
     }
