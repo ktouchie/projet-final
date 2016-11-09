2ad33b2750cad2e9e8bbcc9d548b9450403e85f6
Karin Touchie <ktouchie@gmail.com>
2016-11-09 11:38:19 +0100

    Create Login service, using email as username

---
+++ src/main/java/co/simplon/reserve/service/LoginService.java
@@ -0,0 +1,43 @@
+package co.simplon.reserve.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.simplon.reserve.model.User;
+import co.simplon.reserve.repository.UserRepository;
+
+public class LoginService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+	User user = userRepository.findByEmail(email);
+	if (user == null) {
+	    throw new UsernameNotFoundException("User name not found");
+	}
+	System.out.println("User : " + user.getEmail());
+	return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+		user.isEnabled(), true, true, true, getGrantedAuthorities(user));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(User user) {
+	List<GrantedAuthority> authorities = new ArrayList<>();
+	String role = user.getRole().toString();
+	System.out.println("Role : " + role);
+	authorities.add(new SimpleGrantedAuthority(role));
+	return authorities;
+    }
+
+}
