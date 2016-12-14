c9666eeec0a16351470873b543416ce0e883468f
Karin Touchie <ktouchie@gmail.com>
2016-12-14 22:15:01 +0100

    Initiate Admin profile in new distribution

--- src/main/java/co/simplon/reserve/web/LoginController.java
+++ src/main/java/co/simplon/reserve/web/LoginController.java
@@ -1,8 +1,11 @@
 package co.simplon.reserve.web;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -11,11 +14,25 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import co.simplon.reserve.model.User;
+import co.simplon.reserve.model.User.Role;
+import co.simplon.reserve.service.UserService;
+
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/login")
     public ModelAndView login(ModelMap model) {
+	// If database is empty, create admin profile
+	List<User> userList = userService.getAll();
+	model.addAttribute("userList", userList);
+	if (userList.isEmpty()) {
+	    User user = new User("Admin", "Admin", "admin", "admin", Role.ADMIN, true);
+	    userService.add(user);
+	}
 	model.addAttribute("isLoginPage", true);
 	return new ModelAndView("login", model);
     }
